7e732dce4c1c86e8384afc1781e702281d1f5bf6
Javiera Guedes <javierahermosa@yahoo.com>
2014-02-24 23:26:16 -0800

    basics

--- app.py
+++ app.py
@@ -21,6 +21,10 @@ def page_not_found(e):
 def index():
     return render_template('index.html')
     
+@app.route("/basics")
+def basics():
+    return render_template('basics.html')
+    
 if __name__ == "__main__":
     port = int(os.environ.get("PORT",5000))
     app.run(host='0.0.0.0', port=port)
